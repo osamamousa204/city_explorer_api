1c8e72793d1154ced646cfe4f7fed5a8edebdb1a
osamamousa204 <osamamousa204@gmail.com>
2020-04-06 19:27:23 +0300

    trails is done

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/location', locationHandler)
 ////////creating the locationHandler\\\\\\\\
 function locationHandler(Request, Response) {
     const city = Request.query.city;
-   getlocation(city)
+    getlocation(city)
         .then((newLocation) => {
             Response.status(200).json(newLocation)
         })
@@ -59,8 +59,6 @@ function locationHandler(Request, Response) {
 function getlocation(city) {
     const KEY = process.env.GEOCODE_API_KEY;
     const URL = `https://us1.locationiq.com/v1/search.php?key=${KEY}&q=${city}&format=json`;
-    console.log('ddddddddmmmmmd', URL);
-
     return superagent.get(URL)
         .then((geoData) => {
             const newLocation = new AllLocation(city, geoData.body);
@@ -72,106 +70,111 @@ function getlocation(city) {
 
 
 /////////A constructor crating an objects of cities locations\\\\\\\\\\\
-
+var lat ;
+var lon ;
 function AllLocation(city, geoData) {
     this.city = city;
     this.display_name = geoData[0].display_name;
     this.latitude = geoData[0].lat;
     this.longitude = geoData[0].lon;
+    lat = this.latitude = geoData[0].lat;
+    lon = this.longitude = geoData[0].lon;
 }
 
 ///////////////////////////////////////////create a weather route\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// let arrWeather = [];
-// //creating a finction to get the route and using the weatherHandler as callbackfunctin\\
+let arrWeather = [];
+//creating a finction to get the route and using the weatherHandler as callbackfunctin\\
 
-// app.get('/weather', weatherHandler)
+app.get('/weather', weatherHandler)
 
 
 
-// ////////creating the locationHandler\\\\\\\\
+////////creating the locationHandler\\\\\\\\
 
-// function weatherHandler(Request, Response) {
-//     const city = Request.query.search_query;
-//     getWeather(city)
-//         .then(arrWeather => {
-//             Response.status(200).json(arrWeather)
-//         })
-// }
+function weatherHandler(Request, Response) {
+    const city = Request.query.search_query;
+    getWeather(city)
+        .then(arrWeather => {
+            Response.status(200).json(arrWeather)
+        })
+}
 
-// /////creating a finction to return  the data as a promise function "so i have to retrive these data using the promise way which is the then after calling the calling" \\\\\\
+/////creating a finction to return  the data as a promise function "so i have to retrive these data using the promise way which is the then after calling the calling" \\\\\\
 
-// function getWeather(city) {
-//     const KEY = process.env.WEATHER_API_KEY;
-//     const URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${KEY}`;
-//     console.log('dddddddddddddddd', URL);
+function getWeather(city) {
+    const KEY = process.env.WEATHER_API_KEY;
+    const URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${KEY}`;
+    return superagent.get(URL)
+        .then(weatherData => {
+            arrWeather = [];
+            arrWeather = weatherData.body.data.map(val => {
+                return new Allweather(val)
+            });
+            return arrWeather
+        })
+}
 
-//     return superagent.get(URL)
-//         .then(weatherData => {
-//             arrWeather = [];
-//             arrWeather = weatherData.body.data.map(val => {
-//                 return new Allweather(val)
-//             });
-//             return arrWeather
-//         })
+/////////A constructor crating an objects of cities locations\\\\\\\\\\\
 
-// }
+function Allweather(val) {
+    this.forecast = val.weather.description;
+    this.time = this.time = new Date(val.valid_date).toString().slice(0, 15);
 
-// /////////A constructor crating an objects of cities locations\\\\\\\\\\\
-
-// function Allweather(val) {
-//     this.forecast = val.weather.description,
-//         this.time = this.time = new Date(val.valid_date).toString().slice(0, 15);
-
-// }
+}
 
 ///////////////////////////////////////////create a trail route\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-// let arrTeail = [];
-// //creating a finction to get the route and using the trailrHandler as callbackfunctin\\
+let arrTeail = [];
+//creating a finction to get the route and using the trailrHandler as callbackfunctin\\
 
-// app.get('/trails', trailHandler)
+app.get('/trails', trailHandler)
 
 
 
-// ////////creating the locationHandler\\\\\\\\
+////////creating the locationHandler\\\\\\\\
 
-// function trailHandler(Request, Response) {
-//     const city = Request.query.search_query;
-//     getTrail(city)
-//         .then(arrTeail => {
-//             Response.status(200).json(arrTeail)
-//         })
-// }
+function trailHandler(Request, Response) {
+    const city = Request.query.search_query;
+    getTrail(city)
+        .then(arrTeail => {
+            Response.status(200).json(arrTeail)
+        })
+}
 
-// /////creating a finction to return  the data as a promise function "so i have to retrive these data using the promise way which is the then after calling the calling" \\\\\\
+/////creating a finction to return  the data as a promise function "so i have to retrive these data using the promise way which is the then after calling the calling" \\\\\\
 
-// function getTrail(city) {
-//     const KEY = process.env.TRAIL_API_KEY;
-//     const URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${KEY}`;
-//     console.log('dddddddddddddddd', URL);
+function getTrail(city) {
+    const KEY = process.env.TRAIL_API_KEY;
+    const URL = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${KEY}`;
+    console.log( 'urllllllllllll', URL);
+    console.log( 'laaaan', lat);
+    console.log( 'looon', lon);
+    return superagent.get(URL)
+        .then(trailData => {
+            arrTeail = [];
+            arrTeail = trailData.body.trails.map(val => {
+                console.log(val);
+                return new Alltrails(val)
+                
+            });
+            return arrTeail
+        })
 
-//     return superagent.get(URL)
-//         .then(weatherData => {
-//             arrTeail = [];
-//             arrTeail = weatherData.body.data.map(val => {
-//                 return new Alltrails(val)
-//             });
-//             return arrTeail
-//         })
+}
 
-// }
+/////////A constructor crating an objects of cities locations\\\\\\\\\\\
 
-// /////////A constructor crating an objects of cities locations\\\\\\\\\\\
-
-// function Alltrails(val) {   
-//         this.name
-//         this.location
-//         this.length
-//         this.stars
-//         this.name
-//         this.name
-//         this.name
-//         this.name
-// }
+function Alltrails(val) {   
+        this.name = val.name;
+        this.location = val.location;
+        this.length = val.length;
+        this.stars = val.stars;
+        this.star_votes = val.starVotes;
+        this.summary = val.summary;
+        this.trail_url = val.url;
+        this.conditions = val.conditionStatus;
+        this.condition_date = val.conditionDate.toString().slice(0 , 9);
+        this.condition_time = val.conditionDate.toString().slice(11 , 8);
+}
 
 
 
